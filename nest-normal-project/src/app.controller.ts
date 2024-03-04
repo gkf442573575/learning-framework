13004f911bc340b177fc30c5f34a7580079e8f12
郭凯峰 <guokaifeng@inspur.com>
2024-03-04 10:15:17 +0800

    update

--- nest-normal-project/src/app.controller.ts
+++ nest-normal-project/src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Body, Post, Res, Get } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
